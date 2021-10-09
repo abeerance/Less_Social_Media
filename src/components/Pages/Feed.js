e8949a54178aed3988c782c6e4d60c45bc73d674
Hadrian Chio <hadrian.chio@gmail.com>
2021-10-09 23:53:20 +0200

    axios response passing as props to feed and childcomponent working

--- src/components/Pages/Feed.js
+++ src/components/Pages/Feed.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import classes from './Feed.module.css'
 import PublicFeed from "../UI/PublicFeed";
+import axios from "axios";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Feed = () => {
+    const { rootState } = useContext(AuthContext);
+    const { isAuth, theUser } = rootState;
+    const [feedback, setFeedback] = useState([]);
+
+    useEffect(() => {
+        const getFeedback = () => {
+            if (isAuth) {
+                const username = theUser.username;
+                // php url
+                let url = "http://127.0.0.1/less_webapp/api/get-posts-feed.php";
+                // sending upload request
+    
+                // new formData
+                const formData = new FormData();
+                formData.append("username", username)
+    
+                // post request
+                axios
+                    .post(url, formData, {
+                        // receive two paramenter endpoint url
+                    })
+                    .then((response) => {
+                        // handle success
+                        setFeedback(response.data);
+                    })
+                    .catch((response) => {
+                        //handle error
+                        console.log(response);
+                    });
+            }
+        };
+        getFeedback();
+        console.log(feedback)
+    }, [rootState, isAuth, theUser]);
+    
+
     return (
         <div className={classes.feedSection}>
-            <PublicFeed />
+            <PublicFeed feedback={feedback}/>
         </div>
     )
 }
--- src/components/UI/PublicFeed.js
+++ src/components/UI/PublicFeed.js
@@ -1,68 +1,28 @@
-import React, { useState, useContext, useEffect } from "react";
+import React from "react";
 import classes from "./PublicFeed.module.css";
 import LikeAndDesc from "./LikeAndDesc";
 import AvatarContainer from "./AvatarContainer";
 import Modal from "@mui/material/Modal";
-import axios from "axios";
-import { AuthContext } from "../../contexts/AuthContext";
 
-const PublicFeed = () => {
-
-    const { rootState } = useContext(AuthContext);
-    const { isAuth, theUser } = rootState;
-    const [feedback, setFeedback] = useState("");
-
-    //window.addEventListener('load', this.onLoadHandler);
-
-    
-
-    useEffect(() => {
-        const getFeedback = () => {
-            if (isAuth) {
-                const username = theUser.username;
-                // php url
-                let url = "http://127.0.0.1/less_webapp/api/get-posts-feed.php";
-                // sending upload request
-    
-                // new formData
-                const formData = new FormData();
-                formData.append("username", username)
-    
-                // post request
-                axios
-                    .post(url, formData, {
-                        // receive two paramenter endpoint url
-                    })
-                    .then((response) => {
-                        // handle success
-                        setFeedback(response.data);
-                        
-                    })
-                    .catch((response) => {
-                        //handle error
-                        console.log(response);
-                    });
-            }
-        };
-        getFeedback();
-    }, [rootState, isAuth, theUser]);
-
-    return (
-        <div className={classes.feedContainer}>
-            <AvatarContainer />
-            <div className={classes.imageContainer}>
-                <h2>this is the image</h2>
-            </div>
-            <LikeAndDesc />
-            <div className={classes.commentContainer}>
-                <div className={classes.commentContent}>
-                    <p className={classes.fuckingP}>
-                        <span className={classes.userName}>Abeerance</span>{" "}
-                        asdijasidojasiojdioajsidjasiodjoiajdoijiodiadiojsadoij
-                    </p>
-                </div>
+const FeedItem = ({ item }) => (
+    <div className={classes.feedContainer}>
+        <AvatarContainer>Abeerance</AvatarContainer>
+        <div className={classes.imageContainer}>
+            <h2>this is the image</h2>
+        </div>
+        <LikeAndDesc />
+        <div className={classes.commentContainer}>
+            <div className={classes.commentContent}>
+                <p className={classes.fuckingP}>
+                    <span className={classes.userName}>Abeerance</span>
+                    {item.post_desc}
+                </p>
             </div>
         </div>
-    );
+    </div>
+);
+
+const PublicFeed = ({ feedback }) => {
+    return feedback.map((f) => <FeedItem item={f} />);
 };
 export default PublicFeed;
