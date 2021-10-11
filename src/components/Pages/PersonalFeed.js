7eeae882d0866dfb43345648dbeabcd94a356b59
Hadrian Chio <hadrian.chio@gmail.com>
2021-10-11 15:22:49 +0200

    deleted some stuff, placeholder images on personal feed for now

--- src/components/Pages/PersonalFeed.js
+++ src/components/Pages/PersonalFeed.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
-import { AuthContext } from '../../contexts/AuthContext';
-import classes from './PersonalFeed.module.css'
+import { AuthContext } from "../../contexts/AuthContext";
+import classes from "./PersonalFeed.module.css";
 import axios from "axios";
 import LoggedInUser from "../UI/LoggedInUser";
 import AvatarPersonalPicture from "../UI/AvatarPersonalPicture";
@@ -11,49 +11,49 @@ import PersonalFeedPhotos from "../UI/PersonalSection/PersonalFeedPhotos";
 const PersonalFeed = () => {
     const { rootState } = useContext(AuthContext);
     const { isAuth, theUser } = rootState;
-    const [user, setUser] = useState()
+    const [user, setUser] = useState();
 
     useEffect(() => {
         const getUsername = () => {
-            if(isAuth){
-                const username = theUser.username
-        
-                let url = "http://127.0.0.1/less_webapp/api/get-avatar-user.php";
-        
-                const formData = new FormData()
-                formData.append("username", username)
-        
+            if (isAuth) {
+                const username = theUser.username;
+
+                let url =
+                    "http://127.0.0.1/less_webapp/api/get-avatar-user.php";
+
+                const formData = new FormData();
+                formData.append("username", username);
+
                 // post request
                 axios
-                .post(url, formData, {
-                    // receive two paramenter endpoint url
-                })
-                .then((response) => {
-                    // handle success
-                    setUser(response.data);
-                })
-                .catch((response) => {
-                    //handle error
-                    console.log(response);
-                });
+                    .post(url, formData, {
+                        // receive two paramenter endpoint url
+                    })
+                    .then((response) => {
+                        // handle success
+                        setUser(response.data);
+                    })
+                    .catch((response) => {
+                        //handle error
+                        console.log(response);
+                    });
             }
-        }
-        getUsername()
-    }, [rootState, isAuth, theUser])
-
+        };
+        getUsername();
+    }, [rootState, isAuth, theUser]);
 
     return (
         <div className={classes.personalSection}>
             <div className={classes.userinfo}>
                 <AvatarPersonalPicture />
                 <div>
-                    <LoggedInUser user={user}/>
+                    <LoggedInUser user={user} />
                     <AvatarInfoContainer />
                 </div>
             </div>
             <EditProfileButton />
             <div className={classes.profileLine} />
-            <PersonalFeedPhotos user={user}/>
+            <PersonalFeedPhotos user={user} />
         </div>
     );
 };
--- src/components/UI/PersonalSection/PersonalFeedPhotos.js
+++ src/components/UI/PersonalSection/PersonalFeedPhotos.js
@@ -1,19 +1,25 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../../contexts/AuthContext";
 import classes from "./PersonalFeedPhotos.module.css";
-import FeedPhotos from "./FeedPhotos";
 import Modal from "@mui/material/Modal";
 import axios from "axios";
+import testimage from '../../../user-assets/sample1.jpeg'
+
+const FeedPhotos = ({ images }) => {
+    <div className={classes.imageContent} />;
+};
 
 const PersonalFeedPhotos = ({ user }) => {
     const { rootState } = useContext(AuthContext);
     const { isAuth, theUser } = rootState;
     const [allImages, setAllImages] = useState([]);
+    const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
         const getAllUserImages = () => {
             if (isAuth) {
                 const username = { user };
+                console.log(username)
 
                 let url = "http://127.0.0.1/less_webapp/api/get-all-images.php";
 
@@ -28,6 +34,8 @@ const PersonalFeedPhotos = ({ user }) => {
                     .then((response) => {
                         // handle success
                         setAllImages(response.data);
+                        setLoaded(true)
+                        console.log(allImages)
                     })
                     .catch((response) => {
                         //handle error
@@ -36,13 +44,57 @@ const PersonalFeedPhotos = ({ user }) => {
             }
         };
         getAllUserImages();
-    }, [rootState, isAuth, theUser]);
+    }, [loaded]);
 
-    return (
-        <div className={classes.imageContainer}>
-            <FeedPhotos allImages={allImages} />
-        </div>
-    );
+    if(!undefined) {
+        return(
+            <div className={classes.imagesGrid}>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+                <div className={classes.imageContainer}>
+                    <img src={testimage} className={classes.imageContent} alt="" />
+                </div>
+            </div>
+        )
+
+        /*return(
+            <div className={classes.imagesGrid}>
+                {allImages.map(images=>(i) => <div>hehehehe</div>)}
+            </div>
+        )*/
+    //return allImages.map((i) => <FeedPhotos images={i} />);
+    }
 };
 
 export default PersonalFeedPhotos;
