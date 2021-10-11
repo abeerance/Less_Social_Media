81571cfcab17c841da664f500f34ec6d00483c5e
Hadrian Chio <hadrian.chio@gmail.com>
2021-10-11 20:41:12 +0200

    added modal blueprint for selecting images on personal profile

--- src/components/UI/PersonalSection/PersonalFeedPhotos.js
+++ src/components/UI/PersonalSection/PersonalFeedPhotos.js
@@ -3,18 +3,18 @@ import { AuthContext } from "../../../contexts/AuthContext";
 import classes from "./PersonalFeedPhotos.module.css";
 import Modal from "@mui/material/Modal";
 import axios from "axios";
-import testimage from '../../../assets/sample1.jpeg'
-import testimage2 from '../../../assets/sample2.jpg'
-import testimage3 from '../../../assets/sample3.jpg'
-import testimage4 from '../../../assets/sample4.jpg'
-import testimage5 from '../../../assets/sample5.jpg'
-import testimage6 from '../../../assets/sample6.jpg'
-import testimage7 from '../../../assets/sample7.jpg'
-import testimage8 from '../../../assets/sample8.jpg'
-import testimage9 from '../../../assets/sample9.jpg'
-import testimage10 from '../../../assets/sample10.jpg'
-import testimage11 from '../../../assets/sample11.jpg'
-import testimage12 from '../../../assets/sample12.jpg'
+import testimage from '../../../assets/sample1.webp'
+import testimage2 from '../../../assets/sample2.webp'
+import testimage3 from '../../../assets/sample3.webp'
+import testimage4 from '../../../assets/sample4.webp'
+import testimage5 from '../../../assets/sample5.webp'
+import testimage6 from '../../../assets/sample6.webp'
+import testimage7 from '../../../assets/sample7.webp'
+import testimage8 from '../../../assets/sample8.webp'
+import testimage9 from '../../../assets/sample9.webp'
+import testimage10 from '../../../assets/sample10.webp'
+import testimage11 from '../../../assets/sample11.webp'
+import testimage12 from '../../../assets/sample12.webp'
 
 
 const FeedPhotos = ({ images }) => {
@@ -22,6 +22,9 @@ const FeedPhotos = ({ images }) => {
 };
 
 const PersonalFeedPhotos = ({ user }) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     const { rootState } = useContext(AuthContext);
     const { isAuth, theUser } = rootState;
     const [allImages, setAllImages] = useState([]);
@@ -61,42 +64,43 @@ const PersonalFeedPhotos = ({ user }) => {
     if(!undefined) {
         return(
             <div className={classes.imagesGrid}>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage2} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage3} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage4} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage5} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage6} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage7} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer}onClick={handleOpen}>
                     <img src={testimage8} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage9} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage10} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage11} className={classes.imageContent} alt="" />
                 </div>
-                <div className={classes.imageContainer}>
+                <div className={classes.imageContainer} onClick={handleOpen}>
                     <img src={testimage12} className={classes.imageContent} alt="" />
                 </div>
+                <Modal open={open} onClose={handleClose}></Modal>
             </div>
         )
 
